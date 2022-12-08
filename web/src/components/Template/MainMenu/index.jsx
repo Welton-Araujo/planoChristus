be2134815a91c406768a2ec25aa0ab995fe0345d
joaojunior7cc <joaojuniordev@gmail.com>
2022-12-07 22:02:48 -0300

    :ok_hand: style: Customization(SideBar Glass)

--- web/src/components/Template/MainMenu/index.jsx
+++ web/src/components/Template/MainMenu/index.jsx
@@ -1,20 +1,24 @@
 import { Link } from 'react-router-dom'
 
 const MainMenu = (props) =>{
-    const { 
+    const {
+        className="mainMenu", 
         menu=[], 
         onClose=undefined, 
+        style={}
     } = props
     // console.log('MainMenu', onClose)
 
     return(
-        <>{
+        <div className={className} style={style}>
+        {
             menu.map((item,i)=>(
                 <Link key={`item-${i}`} to={item.src} onClick={onClose}>
                     <span className={item.icon}> {item.description}</span>
                 </Link>
             ))
-        }</>
+        }
+        </div>
     )
 }
 
